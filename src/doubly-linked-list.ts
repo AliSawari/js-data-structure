6e0cc840de5c6755d380d64bff4d79486ff3be05
Ali Sawari <ali.dragon.swr@gmail.com>
2024-10-26 22:09:58 +0300

    chore: Doubly Linked Lists files

--- src/doubly-linked-list.ts
+++ src/doubly-linked-list.ts
@@ -13,21 +13,21 @@ export class Node implements NodeType {
   constructor(value: any) {
     this.value = value;
     this.next = null;
-    this.previous = null;
+    this.previous = null;     
   }
 }
 
 
 export class DoublyLinkedList implements LinkedListType {
-  length: number;
-  head: NodeType;
-  tail: NodeType;
+  length: number = 0;
+  head: NodeType | null;
+  tail: NodeType | null;
 
   constructor(value?: any) {
     this.head = null;
     this.tail = null;
     if (value) {
-      const newNode = new Node(value)
+      const newNode = new Node(value);
       this.head = newNode;
       this.tail = newNode;
       this.increment();
@@ -44,7 +44,6 @@ export class DoublyLinkedList implements LinkedListType {
     }
   }
 
-
   set(index: number, value: any) {
     let temp = this.get(index);
     if (temp) {
@@ -53,15 +52,103 @@ export class DoublyLinkedList implements LinkedListType {
     } else return false;
   }
 
-  get(index: number): NodeType {
-    if (!this.head || index >= this.length) return undefined;
+  get(index: number): NodeType | null {
+    if (!this.head || index >= this.length) return null;
     if (index === 0) return this.head;
     if (index === (this.length - 1)) return this.tail;
-    let pointer = this.head;
-    for (let x = 0; x < index; x++) {
-      if (pointer.next) pointer = pointer.next;
+    let pointer: NodeType | null;
+
+    let middle = Math.ceil(this.length / 2);
+    if((index+1) <= middle) {
+      pointer = this.head;
+      for (let x = 0; x < index; x++) {
+        if (pointer.next) pointer = pointer.next;
+      }
+    } else {
+      pointer = this.tail;
+      for(let x = (this.length-1); x > index; x--){
+        if (pointer.previous) pointer = pointer.previous;
+      }
     }
+    
     return pointer;
   }
 
+
+  push(value: any): LinkedListType {
+    const newNode = new Node(value);
+    if (this.length == 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+
+    this.increment();
+    return this;
+  }
+
+
+  
+  pop(): NodeType | null {
+    if (!this.head) return null;
+    let pointer: NodeType;
+    if (this.length == 1) {
+      pointer = this.head;
+      this.head = null;
+      this.tail = null;
+    } else {
+      pointer = this.tail;
+      this.tail = pointer.previous;
+      this.tail.next = null;
+    }
+    pointer.next = null;
+    pointer.previous = null;
+    this.decrement();
+    return pointer;
+  }
+
+
+
+  shift(): NodeType | unknown {
+    throw new Error('Method not implemented.')
+  }
+  unshift(value: any): LinkedListType {
+    throw new Error('Method not implemented.')
+  }
+  insert(index: number, value: any): void {
+    throw new Error('Method not implemented.')
+  }
+  remove(value: any): NodeType | unknown {
+    throw new Error('Method not implemented.')
+  }
+  reverse(): void {
+    throw new Error('Method not implemented.')
+  }
+
+
+
+  log(): void {
+    console.clear()
+    console.log("Linked List Tree");
+    if(!this.head){
+      console.log("List is Empty!");
+      console.log("Length: ", this.length);
+      return;
+    }
+    console.log("Head:", this.head.value);
+    console.log("Tail:", this.tail.value);
+    console.log("Length:", this.length);
+    console.log("#######")
+    let pointer = this.head;
+    for(let i = 0; i < this.length; i++){
+      console.log("~",i);
+      console.log("value:", pointer.value);
+      console.log("next:", pointer.next ? pointer.next.value : undefined);
+      pointer = pointer.next;
+      console.log("----")
+    }
+    
+  }
 }
---
+++ src/examples/doubly-linked-list.ts
@@ -0,0 +1,14 @@
+import { DoublyLinkedList } from '../doubly-linked-list';
+
+const myDoubly = new DoublyLinkedList();
+
+
+myDoubly.push("Reza")
+myDoubly.push("Hamid")
+myDoubly.push("Mosa")
+myDoubly.push("Mohammad")
+myDoubly.push("Taher")
+myDoubly.push("Jesus")
+myDoubly.pop();
+myDoubly.insert(7, "Jafar")
+myDoubly.log()
